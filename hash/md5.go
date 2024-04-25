414c65cd8fb29d2cdb27551917eceb9b320dd111
TanH <36893804+Tanh-wink@users.noreply.github.com>
2024-04-25 11:23:15 +0800

    Update md5.go

--- hash/md5.go
+++ hash/md5.go
@@ -29,13 +29,5 @@ func FileMd5(filename string) (string, error) {
 func StringMd5(s string) string {
     md5 := md5.New()
     md5.Write([]byte(s))
-	fmt.Println("hello word")
     return hex.EncodeToString(md5.Sum(nil))
-}
-
-
-// get string md5
-func Fun() int {
-    
-    return 5
 }
